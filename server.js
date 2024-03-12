93be84f6e43fdb948c08374c21fdd8593b1684ec
kishorekumar1111 <kishore.kumar@kalvium.community>
2024-03-12 12:09:14 +0530

    bruno

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-// Importing necessary modules
+
 const express = require('express');
 
-// Creating an instance of express app
-const app = express();
-const port = 3000; // Port number
 
-// Middleware to parse JSON bodies
+const app = express();
+const port = 3000; 
+
+
 app.use(express.json());
 
-// Route handler for GET request to "/ping"
+
 app.get('/ping', (req, res) => {
-  res.send('Pong!'); // Sending "Pong!" response
+  res.send('Pong!'); 
 });
 
 // Starting the server
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
+
